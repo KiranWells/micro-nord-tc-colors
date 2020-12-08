2120dd146d09c10165c16598dd3dd86371c7c3ec
Kiran Wells <64935184+KiranWells@users.noreply.github.com>
2020-12-08 15:06:15 -0600

    Add the light themes

--- nord-tc.lua
+++ nord-tc.lua
@@ -2,3 +2,5 @@ VERSION = "1.1.0"
 
 AddRuntimeFile("nord-tc", "colorscheme", "nord-tc.micro")
 AddRuntimeFile("nord-16", "colorscheme", "nord-16.micro")
+AddRuntimeFile("nord-tc-light", "colorscheme", "nord-tc-light.micro")
+AddRuntimeFile("nord-16-light", "colorscheme", "nord-16-light.micro")
